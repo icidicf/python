d8931a81bc09f9794383d5409f596b0f27c5ed24
icidicf <icidicf@gmail.com>
2015-05-21 21:56:29 -0700

    add telnet script

---
+++ sys_amdin/network/telnet.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import getpass 
+import sys
+import telnetlib
+
+host = "sh-cable-lotus"
+user = "jelou"
+password = "lab123"
+
+tn =telnetlib.Telnet(host)
+
+tn.read_until("login:")
+tn.write(user + "\n")
+
+tn.read_until("Password:")
+tn.write(password + "\n")
+
+tn.write("ls\n");
+tn.write("echo \"welcome Yangping\"\n");
+
+##### interactive mode ############
+tn.interact();
+
+
+#########do job and exit################
+#tn.write("exit\n");
+#print tn.read_all()
+
+
+
