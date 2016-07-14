449ee4c350e75970d2a9c443320befc5f3fedd5e
icidicf <icidicf@gmail.com>
2016-07-14 10:47:56 +0800

    add timestamp plot

---
+++ basic/graph/Pylot/timestamp.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as md
+import numpy as np
+import datetime as dt
+import time
+
+n=20
+duration=24*3600*n
+now=time.mktime(time.localtime())
+timestamps=np.linspace(now,now+duration,n)
+dates=[dt.datetime.fromtimestamp(ts) for ts in timestamps]
+datenums=md.date2num(dates)
+values=np.sin((timestamps-now)/duration*2*np.pi)
+plt.subplots_adjust(bottom=0.2)
+plt.xticks( rotation=25 )
+ax=plt.gca()
+xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
+ax.xaxis.set_major_formatter(xfmt)
+for d in datenums:
+        print d
+        plt.xticks(np.arange(min(datenums), max(datenums), 2.0 ))
+        plt.plot(datenums,values)
+        plt.show()
+
